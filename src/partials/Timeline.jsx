2a59ebabf0fdf5b94a26ce38204df6c5a9beea21
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2023-06-23 17:07:34 +0700

    Update Timeline contents

--- src/partials/Timeline.jsx
+++ src/partials/Timeline.jsx
@@ -77,37 +77,31 @@ function Timeline() {
             <div className='relative xl:-mt-[1.6px] md:-mt-[1.8px] -mt-[1.6px] phase-3 md:px-16 pl-12 py-12'>
                 {/* Phase Title */}
                 <h2 className='text-transparent bg-clip-text bg-gradient-to-t from-green-100 to-green-200 h2 pb-4'>
-                    {t("preliminaryWorkshop")}
+                    {t("mentoringWorkshops")}
                 </h2>
                 {/* Phase button */}
                 <div className='absolute bg-black-100 top-11 left-0 flex content-center transform translate-x-[-50%] h-[60px] w-[40px]'>
                     <img className='w-full object-contain' src={timelineButton} alt='timeline point'></img>
                 </div>
                 {/* Phase Contents */}
-                <h4 className='h4 text-white-100 pb-4'>{t("preliminaryWorkshopDate")}</h4>
-                <p className='p pb-4'>{t("registrationAndSubmission")}</p>
-                <h4 className='h4 text-white-100 pb-4'>{t("workshopQA")}</h4>
-                <p className='p pb-4'>{t("workshopQADescription")}</p>
-                <h4 className='h4 text-white-100 pb-4'>{t("preliminaryResultAnnouncement")}</h4>
-                <p className='p pb-4'>{t("preliminaryResultAnnouncementDetail")}</p>
+                <h4 className='h4 text-white-100 pb-4'>{t("mentoringWorkshopsDate")}</h4>
+                <p className='p pb-4'>{t("topFinalistsMeeting")}</p>
+                <h4 className='h4 text-white-100 pb-4'>{t("workshopForFinalists")}</h4>
+                <p className='p pb-4'>{t("workshopForFinalistsDescription")}</p>
             </div>
             {/* Phase 4 */}
             <div className='relative xl:-mt-[1.6px] md:-mt-[1.8px] -mt-[1.6px] phase-4 text-right md:px-16 pr-12 py-12'>
                 {/* Phase Title */}
                 <h2 className='text-transparent bg-clip-text bg-gradient-to-t from-green-100 to-green-200 h2 pb-4'>
-                    {t("preliminaryWorkshop")}
+                    {t("grandFinale")}
                 </h2>
                 {/* Phase button */}
                 <div className='absolute bg-black-100 top-11 right-0 flex content-center transform translate-x-[50%] h-[60px] w-[40px]'>
                     <img className='w-full object-contain' src={timelineButton} alt='timeline point'></img>
                 </div>
                 {/* Phase Contents */}
-                <h4 className='h4 text-white-100 pb-4'>{t("preliminaryWorkshopDate")}</h4>
-                <p className='p pb-4'>{t("registrationAndSubmission")}</p>
-                <h4 className='h4 text-white-100 pb-4'>{t("workshopQA")}</h4>
-                <p className='p pb-4'>{t("workshopQADescription")}</p>
-                <h4 className='h4 text-white-100 pb-4'>{t("preliminaryResultAnnouncement")}</h4>
-                <p className='p pb-4'>{t("preliminaryResultAnnouncementDetail")}</p>
+                <h4 className='h4 text-white-100 pb-4'>{t("grandFinaleDate")}</h4>
+                <p className='p pb-4'>{t("grandFinalistsPitch")}</p>
             </div>
         </section>
     )
