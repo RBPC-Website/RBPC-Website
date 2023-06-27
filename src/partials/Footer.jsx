5d88483712c2cbdda6ca9440e7ab89c26e345251
Cullen179 <itslamemail@gmail.com>
2023-06-27 16:04:18 +0700

    Update Footer.jsx

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -21,14 +21,14 @@ export default function Footer() {
                                 <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
                             </svg>
                         </span>
-                        <div className="text-base text-left">
+                        <div className="text-base sm:text-left">
                             <p>Hotline: (84-28) 3776 1418 (TP. HCM) | (84-24) 3724 5923 (Hanoi) </p>
                         </div>
 
                     </a>
                 </div>
                 {/* Facebook */}   
-                <div className="flex-1 min-w-[280px] max-w-[340px] flex justify-end mx-auto">
+                <div className="flex-1 min-w-[280px] max-w-[340px] flex justify-end mx-auto sm:mx-0">
                     <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Frmit.rbpc&tabs&width=340&height=70&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" allowFullScreen={true} width={'100%'} height={'70px'} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                 </div>
             </div>
