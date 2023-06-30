54af60db43a6b78ada45f860d55ebfd85e0ae13d
dolelongan <dolelongan@gmail.com>
2023-07-01 00:49:38 +0700

    fixes on UI Header

--- src/utils/Countdown.jsx
+++ src/utils/Countdown.jsx
@@ -68,6 +68,7 @@ export default function Countdown() {
                     <div className="flex items-center">
                         <button data-dismiss-target="#sticky-banner" type="button" onClick={() => {setShowBanner(false)}} className="flex-shrink-0 inline-flex justify-center items-center text-black-200 hover:bg-gray-100 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
                             <svg aria-hidden="true" className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                            CLOSE
                         </button>
                     </div>
                 </div>
